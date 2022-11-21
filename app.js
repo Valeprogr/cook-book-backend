02e5d0fc021cdafa26f2292d0aac2931259e1ba7
Luca <lukkino31@gmail.com>
2022-11-21 15:30:30 +0100

    basic router created

--- app.js
+++ app.js
@@ -3,7 +3,32 @@ const app = express();
 const PORT = 3000;
 
 app.get("/",(req,res,next)=>{
-    res.send("<h1>Hello world!</h1>")
+    res.send(`
+    <h1>Home page</h1>
+    <p>Welcome to our cookbook</p>
+    <ul>
+      <li><a href='/category/:id'>Seefood</a></li>
+      <li><a href='/category/:id'>Maindish</a></li>
+      <li><a href='/category/:id'>Veggy</a></li>
+      <li><a href='/category/:id'>Dessert</a></li>
+    </ul>
+    `)
+})
+
+app.get('/category/:id', (req, res) => {
+    res.send(`
+    <h1>Category page!</h1>
+    <p>Welcome to our category page</p>
+    <h2><a href='/'>Back home</a></h2>
+    `);
+})
+
+app.get('/recipe/:id', (req, res) => {
+    res.send(`
+    <h1>Recipe page!</h1>
+    <p>Welcome to our category page</p>
+    <h2><a href='/'>Back home</a></h2>
+    `);;
 })
 
 app.listen(PORT, ()=>{
