59691f425fbe4f483873bd1273565f367b3949d3
Luca <lukkino31@gmail.com>
2022-11-24 14:27:15 +0100

    basic sceleton to connect to postgreSQL

--- test.js
+++ test.js
@@ -1,20 +1,20 @@
-const { Pool } = require('pg');
-const express = require("express");
-const app = express();
+// const { Pool } = require('pg');
+// const express = require("express");
+// const app = express();
 
-const pool = new Pool({
-    user: '',
-    host: '',
-    database: '',
-    password '',
-    port: ,
-});
+// const pool = new Pool({
+//     user: '',
+//     host: '',
+//     database: '',
+//     password '',
+//     port: ,
+// });
 
-app.get("/", (req,res) => {
-    pool
-      .query('')
-      .then(data => res.json(data.rows))
-      .catch (e => res.sendStatus(500));
-});
+// app.get("/", (req,res) => {
+//     pool
+//       .query('SELECT * FROM recipes;')
+//       .then(data => res.json(data.rows))
+//       .catch (e => res.sendStatus(500));
+// });
 
-app.listen('3000', () => console.log('connected'));
+// app.listen('3000', () => console.log('connected'));
